301e34f95b64bbe10dd402276a00ad75408a5093
Slyrich <richdai@uga.edu>
2015-02-12 13:18:36 -0500

    shell script for CSCI1730 grading

--- shellscript.sh
+++ shellscript.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#to run type bash shellscript.sh > output.txt
+#@author: Rich Dai
+
 for i in `ls *.tar*`; do
 	mkdir ${i%????????} 
 	tar xvf ${i} -C ${i%????????}
@@ -23,14 +26,20 @@ for i in `ls *.tar*`; do
 		echo "----------"
 		gcc -g -o badloops.c badloops
 
-
+		#input1
 		./badloops <<< a
+
+		#input2
 		echo "----------"		
 		./badloops <<< ProgrammingCisAAAwesome
+
+		#input3
 		echo "----------"
 		./badloops <<< aaaaaaaaaaaaaaaahhhhhhhhhhhhhhhh
 	echo "===================="		
 	done
+
+#go back to the dir where you stored all the tar files
 cd /home/grads/dai/CSCI1730/lab2
 done
 
